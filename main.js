6b466be2bec635444fd62837dd01080a6dd5c05b
perim10 <142339368+perim10@users.noreply.github.com>
2024-09-25 12:29:39 -0300

    fgfg

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ function areadoquadrado(base1, altura1){
  function areadotriangulo(base1, altura1,){
     return (base1*altura1) /2 ; }
    
-
-
+let media = function(num1,num2,num3){
+return (num1+num2+num3) / 3 ;}
+console.log()
+let CalculodeMediaDoUsuario = function Mediazinha(num1, num2, num3) {
+    num1 = parseInt(prompt("Me dê o primeiro número"))
+    num2 = parseInt(prompt("Me dê o segundo número"))
+    num3 = parseInt(prompt("Me dê o terceiro número"))
+    return (num1+num2+num3)/3   
+}
+console.log(CalculodeMediaDoUsuario())
 
