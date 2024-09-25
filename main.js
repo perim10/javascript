f35f814ca15e13b7a599cb1401325494005cbacd
perim10 <142339368+perim10@users.noreply.github.com>
2024-09-25 12:02:23 -0300

    rerc

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function areadoquadrado(base1, altura1){
  function areadotriangulo(base1, altura1,){
     return (base1*altura1) /2 ; }
    
-   let media = function lucas(x,y,z){
-    return (x+y+z) /3 ;
+   let media = function lucas(3){
+    return (2) 
    }
    console.log()
