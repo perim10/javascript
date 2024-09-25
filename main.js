fceac23076c1cc8b29e9483bdf211759759c9b45
perim10 <142339368+perim10@users.noreply.github.com>
2024-09-25 11:42:38 -0300

    fdc

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function soma (num1, num2) { return num1+num2 ; } console.log(soma(3,4));
+
 function areadoquadrado(base1, altura1){
  return base1*altura1 ; }
  console.log(areadoquadrado(3,4));
+
+
+ function areadotriangulo(base1, altura1, 2){
+    return base1*altura1/2 ; }
+   
+   
