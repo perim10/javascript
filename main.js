a9d560637bd2d44e5c06c931eeb26fb6f2fb1df4
perim10 <142339368+perim10@users.noreply.github.com>
2024-10-02 12:20:54 -0300

    gbgfb

--- main.js
+++ main.js
@@ -31,17 +31,17 @@
 // }
 
 // console.log(Media(2, 3, 10))
-let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+// let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
 
 
-let i = 0 ;
-while(i< estados.length){
-console.log(i);
-console.log(estados[i]);
-i= i+1 ;
-}
-for (let index = 0; index < estados.length; index++) {
-    console.log(index);
-    console.log(estados[index]);
+// let i = 0 ;
+// while(i< estados.length){
+// console.log(i);
+// console.log(estados[i]);
+// i= i+1 ;
+// }
+// for (let index = 0; index < estados.length; index++) {
+//     console.log(index);
+//     console.log(estados[index]);
 
-}
+// }
