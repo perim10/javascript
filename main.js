8c0a2fe6477ba38764c48e359406cdc3101df885
perim10 <142339368+perim10@users.noreply.github.com>
2024-10-02 12:12:56 -0300

    bhbhb

--- main.js
+++ main.js
@@ -33,9 +33,14 @@
 // console.log(Media(2, 3, 10))
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
 let i = 0 ;
-while(i<estados.length){
+while(i< estados.length){
 
 console.log(i);
 console.log(estados[i]);
 i= i+1 ;
+}
+for (let index=0; estados.length; index++){
+    console.log(index);
+    console.log(estados[i]);
+
 }
