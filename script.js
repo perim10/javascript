59a7fab52d8583f1d0658e0192692f8d4e4ca56b
perim10 <142339368+perim10@users.noreply.github.com>
2024-10-02 11:41:22 -0300

    ffb fcbf

--- script.js
+++ script.js
@@ -31,3 +31,5 @@
 // }
 
 // console.log(Media(2, 3, 10))
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+  
