7c7eb85bd4f050aae2c2352b97dc323351cfae68
perim10 <142339368+perim10@users.noreply.github.com>
2024-10-02 11:22:30 -0300

    cs cx c

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-alert("lucas 23,jorge15")
-function soma (num1, num2) { return num1+num2 ; } console.log(soma(3,4));
+// alert("lucas 23,jorge15")
+// function soma (num1, num2) { return num1+num2 ; } console.log(soma(3,4));
 
 
-function areadoquadrado(base1, altura1){
- return base1*altura1 ; }
- console.log(areadoquadrado(3,4));
+// function areadoquadrado(base1, altura1){
+//  return base1*altura1 ; }
+//  console.log(areadoquadrado(3,4));
 
 
- function areadotriangulo(base1, altura1,){
-    return (base1*altura1) /2 ; }
+//  function areadotriangulo(base1, altura1,){
+//     return (base1*altura1) /2 ; }
    
-let media = function(num1,num2,num3){
-return (num1+num2+num3) / 3 ;}
-console.log()
-let CalculodeMediaDoUsuario = function Mediazinha(num1, num2, num3) {
-    num1 = parseInt(prompt("Me dê o primeiro número"))
-    num2 = parseInt(prompt("Me dê o segundo número"))
-    num3 = parseInt(prompt("Me dê o terceiro número"))
-    return (num1+num2+num3)/3   
-}
-console.log(CalculodeMediaDoUsuario())
+// let media = function(num1,num2,num3){
+// return (num1+num2+num3) / 3 ;}
+// console.log()
+// let CalculodeMediaDoUsuario = function Mediazinha(num1, num2, num3) {
+//     num1 = parseInt(prompt("Me dê o primeiro número"))
+//     num2 = parseInt(prompt("Me dê o segundo número"))
+//     num3 = parseInt(prompt("Me dê o terceiro número"))
+//     return (num1+num2+num3)/3   
+// }
+// console.log(CalculodeMediaDoUsuario())
+let estados=[Acre, Alagoas, Amapá, Amazonas, Bahia, Ceará, Distrito Federal, Espírito Santo, Goiás, Maranhão, Mato Grosso, Mato Grosso do Sul, Minas Gerais, Pará, Paraíba, Paraná, Pernambuco, Piauí, Rio de Janeiro, Rio Grande do Norte, Rio Grande do Sul, Rondônia, Roraima, Santa Catarina, São Paulo, Sergipe, Tocantins]
+
 
